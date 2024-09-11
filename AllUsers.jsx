fa04f6771e4bc89e40bc8a4f31eba9ecaf2430eb
Ashima Saini <37665854+ashimasaini45@users.noreply.github.com>
2024-09-12 02:06:41 +0530

    Add files via upload

---
+++ AllUsers.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+
+const AllUsers=()=>{
+ return(
+   <div className="container">
+<table className="table-primary">...</table>
+<table className="table-secondary">...</table>
+<table className="table-success">...</table>
+<table className="table-danger">...</table>
+<table className="table-warning">...</table>
+<table className="table-info">...</table>
+<table className="table-light">...</table>
+<table className="table-dark">...</table>
+
+
+<tr className="table-primary">...</tr>
+<tr className="table-secondary">...</tr>
+<tr className="table-success">...</tr>
+<tr className="table-danger">...</tr>
+<tr className="table-warning">...</tr>
+<tr className="table-info">...</tr>
+<tr className="table-light">...</tr>
+<tr className="table-dark">...</tr>
+
+
+<tr>
+  <td className="table-primary">...</td>
+  <td className="table-secondary">...</td>
+  <td className="table-success">...</td>
+  <td className="table-danger">...</td>
+  <td className="table-warning">...</td>
+  <td className="table-info">...</td>
+  <td className="table-light">...</td>
+  <td className="table-dark">...</td>
+</tr>
+</div>
+ )
+}
+export default AllUsers;
